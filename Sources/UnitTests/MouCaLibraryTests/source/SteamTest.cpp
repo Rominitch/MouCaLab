0d2fee18f86b612b38d49bdb0981f732cbcab045
Rominitch <rominitch@gmail.com>
2020-12-02 12:21:08 +0100

    [FIX] Generate Steam dummy ID for testing.

--- Sources/UnitTests/MouCaLibraryTests/source/SteamTest.cpp
+++ Sources/UnitTests/MouCaLibraryTests/source/SteamTest.cpp
@@ -4,6 +4,13 @@
 
 void MouCaSteam::SetUp()
 {
+    {
+        Core::File steamAppId("steam_appID.txt");
+        steamAppId.open(L"w");
+        steamAppId.writeText("480");
+        steamAppId.close();
+    }
+
     ASSERT_NO_THROW(_platform.initialize()) << "Steam must be launched before gtest";
 }
 
